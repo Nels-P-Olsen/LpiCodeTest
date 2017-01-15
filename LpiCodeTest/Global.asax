1803fa6bb91b1a02cb0b11b84fe0c6d454a7dcdb
Nels Olsen <nels.olsen@sbcglobal.net>
2017-01-15 15:17:43 -0600

    Add project files.

---
+++ LpiCodeTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LpiCodeTest.MvcApplication" Language="C#" %>
---
+++ LpiCodeTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LpiCodeTest.Startup))]
+namespace LpiCodeTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
